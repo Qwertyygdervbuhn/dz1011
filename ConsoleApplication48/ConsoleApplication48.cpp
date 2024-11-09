4598388edfb218b1eb3e2d6d6ed80ca7ede9293f
Alexander Dm. <dabl21291@gmail.com>
2024-11-10 02:00:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication48/ConsoleApplication48.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+template<class T>
+class MyArray {
+    T* mas;
+    int size;
+public:
+    MyArray();
+    void Output();
+    ~MyArray();
+};
+
+template<class T>
+MyArray<T>::MyArray() {
+    size = 10;
+    mas = new T[size];
+    for (int i = 0; i < size; i++) {
+        mas[i] = T(); 
+    }
+}
+
+template<class T>
+void MyArray<T>::Output() {
+    for (int i = 0; i < size; i++) {
+        cout << mas[i] << "\t";
+    }
+    cout << endl;
+}
+
+template<class T>
+MyArray<T>::~MyArray() {
+    delete[] mas;
+}
+
+class Point {
+    int x, y;
+public:
+    Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+    friend ostream& operator<<(ostream& os, const Point& p) {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
+    }
+};
+
+int main() {
+ 
+
+    MyArray<int> intArray;
+    cout << "Int Array: ";
+    intArray.Output();
+
+    MyArray<char> charArray;
+    cout << "Char Array: ";
+    charArray.Output();
+
+    MyArray<double> doubleArray;
+    cout << "Double Array: ";
+    doubleArray.Output();
+
+    MyArray<Point> pointArray;
+    cout << "Point Array: ";
+    pointArray.Output();
+
+}
